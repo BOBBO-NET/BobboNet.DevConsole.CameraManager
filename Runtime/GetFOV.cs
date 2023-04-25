4ed13f512b4a86738dc406cab36a8a8f9dfef1ef
LAPTOP-3BPAUQ9G\Iseei <iseeicyhawkston@gmail.com>
2023-04-25 01:29:23 -0500

    Initial Commit

---
+++ Runtime/GetFOV.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using UnityEngine;
+using Zenject;
+
+namespace BobboNet.DevConsole.CameraManager
+{
+    public class GetFOV : DevConsoleCommandBase
+    {
+        public override string CommandName => "get_fov";
+        private CameraManager cameraManager;
+
+        [Inject]
+        public GetFOV(CameraManager cameraManager)
+        {
+            this.cameraManager = cameraManager;
+        }
+
+        public override string ExecuteCommand(string[] args)
+        {
+            return $"FOV: {cameraManager.FOV}";
+        }
+    }
+}
+
